3c382682d51c81be2b810333b13a6870f71937e5
jaragorns <jhonny77111@gmail.com>
2016-03-31 16:02:23 -0500

    Comente el uso de unos iconos que utilice para que fuera más dinámica, pero al realizar el comité por Xcode me ignora las carpetas donde se encuentras dichos iconos y por ende no funcionaria. Pero localmente quedo mucho más amigable igual como el código queda comentada para quien guste puede ver como implemente adicional a un label o button un image y luego por medio del IUImage pude manipular y cambiar la imagen según sea programado.

--- HamburguesasDelMundo/ViewController.swift
+++ HamburguesasDelMundo/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
     let hamburguesa = coleccionDeHamburguesa()
     let color = colores()
     
-    @IBOutlet weak var flag: UIImageView!
-    @IBOutlet weak var hamburger: UIImageView!
+    //@IBOutlet weak var flag: UIImageView!
+    //@IBOutlet weak var hamburger: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,10 +39,10 @@ class ViewController: UIViewController {
         labelHamburguesa.text = hamburguesa.obtenHamburguesa()
         
         view.backgroundColor = colorAleatorio
-        flag.image = UIImage(named: "flags/\(country).png")
+        //flag.image = UIImage(named: "flags/\(country).png")
         
-        let ham = Int(arc4random()) % 4
-        hamburger.image = UIImage(named: "hamburger/\(ham).png")
+        //let ham = Int(arc4random()) % 4
+        //hamburger.image = UIImage(named: "hamburger/\(ham).png")
         
     }
 
